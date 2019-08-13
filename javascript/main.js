79a696e3d90679f6cf0a9e5def67266080734980
daniel <dmjames04@yahoo.com>
2019-08-13 23:23:44 +0100

    updates to file

--- javascript/main.js
+++ javascript/main.js
@@ -15,7 +15,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 10000); // Change image every 2 seconds
+  setTimeout(showSlides, 20000); // Change image every 2 seconds
 }
 
 let mainNav = document.getElementById('js-menu');
